4a7177b3021a66edd15163096930f3ee091932e4
Harold Martin <harold.martin@gmail.com>
2026-09-02 08:55:10 -0700

    setup

---
+++ PartyBox Controller/PartyBox_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyBox_ControllerApp.swift
+//  PartyBox Controller
+//
+//  Created by Harold Martin on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PartyBox_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PartyBox ControllerTests/PartyBox_ControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PartyBox_ControllerTests.swift
+//  PartyBox ControllerTests
+//
+//  Created by Harold Martin on 9/2/26.
+//
+
+import Testing
+@testable import PartyBox_Controller
+
+struct PartyBox_ControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
